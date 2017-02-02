225d7ede0f30c91be72609b9047b1a54dc2a9f99
Jim de Beer <jim@vigour.io>
2017-02-02 22:06:55 +0100

    watcher

--- test/simple/a.js
+++ test/simple/a.js
@@ -1,26 +1,3 @@
-import bb from './b'
-import * as success from './b'
-import { a } from './b'
-require('./a')
-
-var obj = {
-  foo: 'baz'
-}
-
-const haha = {
-  bla: obj
-}
-const { a, b } = 'blur'
-
-var x = {}
-x.blarf = true
-console.log(x.blarf)
-
-function smurk (a, b) {
-  console.log(a, b)
-  console.log()
-}
-
-smurk()
-
-export default 'a!' + bb
+var a = 'bla'
+require('ballz')
+yuzi()
