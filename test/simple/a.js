38c00408eedafbc83086a6923ec467b8a175f227
Jim de Beer <jim@vigour.io>
2017-02-03 00:30:53 +0100

    added arrow and anonymous functions

--- test/simple/a.js
+++ test/simple/a.js
@@ -1,6 +1,19 @@
-import bla from './b'
-console.log(bla)
-var a = bla
-// bla is shit
-// export default a
-export default '!'
+import bla from './b';
+// console.log(bla)
+// var a = bla
+// // bla is shit
+// // need to put this in scope
+// var x = true
+
+;(function (a) {
+  var blxa = 'x'
+  console.log(a, bla)
+})()
+
+const x = (a)  => {
+  var b = 'hello'
+  console.log(bla, a, b)
+}
+
+// // export default a
+// // export default '!'
