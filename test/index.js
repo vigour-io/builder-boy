a54fb02efe05769a7eff2e2b43fa677194444cdf
Youri Daamen <me@youz.io>
2017-02-02 22:10:11 +0100

    whut

--- test/index.js
+++ test/index.js
@@ -6,4 +6,18 @@ fs.realpath(__dirname + '/simple/a.js', (err, real) => { // eslint-disable-line
   boy.set({
     [real]: true
   })
+
+  setTimeout(() => {
+    console.log(JSON.stringify(boy.serialize(), false, 2))
+    console.log('::', build(real))
+  }, 500)
 })
+
+function build (filename) {
+  const file = boy.get(filename)
+  var result = file.get('result').compute()
+  file.get('dependencies').keys().forEach(dep => {
+    result = build(dep) + '\n' + result
+  })
+  return result
+}
