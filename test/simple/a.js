952602510b630145d618908b6e34846079a05cb0
Jim de Beer <jim@vigour.io>
2017-02-03 13:11:14 +0100

    export const

--- test/simple/a.js
+++ test/simple/a.js
@@ -1,11 +1,17 @@
-import bla from './b'
+// import bla from './b'
 var a = 1
 
-const x = { a, bla }
+// const x = { a, bla }
 
-export default {
-  bla,
-  a
-}
+// export default {
+//   bla,
+//   a
+// }
+
+// export {
+//   a
+// }
+
+export const b = a
 
 // export default a
