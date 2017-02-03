dfcf888a87b1622a151c0fd902ef94daef4de750
Jim de Beer <jim@vigour.io>
2017-02-03 14:39:25 +0100

    added * imports

--- test/simple/a.js
+++ test/simple/a.js
@@ -1,4 +1,5 @@
 // import bla from './b'
-import { a, b, c as d } from './b'
+// import { a, b, c as d } from './b'
+import * as blurf from './b'
 
-console.log(a, b, d)
+console.log(blurf.a, blurf.b, blurf.d)
