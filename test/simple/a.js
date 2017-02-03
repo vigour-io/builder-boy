c465fff71c90156a084b7a81f5132a1929b57620
Jim de Beer <jim@vigour.io>
2017-02-03 11:47:19 +0100

    expost var replacements

--- test/simple/a.js
+++ test/simple/a.js
@@ -1,39 +1,9 @@
 import bla from './b'
 // console.log(bla)
 var a = bla
-// bla is shit
-// need to put this in scope
-
-;(function (a) {
-  var bla = 'x'
-  console.log(a, bla)
-})()
-
-const x = true
-
-var y = () => bla
-
-var bla = {
-  hello (x) {
-    console.log('x', x, bla)
-  }
-}
-
-class StyleSheet {
-  constructor (t, x) {
-    console.log(t, bla, x, y)
-  }
-}
-
-function dirt (a = x, b = bla, { c: g, d }) {
-  var b = x
-  var c = b
-  var d = bla
-}
-
-for (var i = 0; i < 10; i++) {
-  console.log(bla)
-}
+// now exports with vars etc
+// export default a
+export default a
 
 // // export default a
 // // export default '!'
