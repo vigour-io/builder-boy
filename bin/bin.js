2524d822968311cfa85c9e31ec90757be0fdb16a
Jim de Beer <jim@vigour.io>
2017-02-03 16:23:08 +0100

    gracefull error handeling

--- bin/bin.js
+++ bin/bin.js
@@ -5,7 +5,11 @@ const chalk = require('chalk')
 const fs = require('fs')
 build(file, (err, code) => {
   if (err) {
-    console.log(err)
+    if (err.file) {
+      console.log(`   ${chalk.red(err.message)} in file ${chalk.red(err.file)} `)
+    } else {
+      console.log(err)
+    }
   } else {
     if (dest) {
       fs.writeFile(dest, code, err => {
