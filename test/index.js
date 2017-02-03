68965af132a970d193b638c20110b5d6f338be5a
Jim de Beer <jim@vigour.io>
2017-02-03 22:09:02 +0100

    correct test

--- test/index.js
+++ test/index.js
@@ -13,18 +13,25 @@ var cnt = 0
 // })
 
 build('../brisky-struct/src/index.js', (err, result) => {
-  cnt++
-  fs.writeFileSync(`./test/real/dist/${cnt}.js`, result)
+  console.log('\n\n\nw0000t')
   if (err) {
-    console.log(err)
-  }
-  // console.log('??? result:', result)
+    console.log('ERROR', !!result, cnt, err.file)
+    return
+  } else {
+    // console.log('??? result:', result)
+    cnt++
+    fs.writeFileSync(`./test/real/dist/${cnt}.js`, result)
 
-  // console.log('\n\n\ngo run script!!!!\n')
-  console.log('REQUIRE:')
-  console.log('---------------------------------------------')
-  require(`./real/dist/${cnt}.js`)
-  console.log('---------------------------------------------')
+    // console.log('\n\n\ngo run script!!!!\n')
+    console.log('REQUIRE:')
+    console.log('---------------------------------------------')
+    try {
+      require(`./real/dist/${cnt}.js`)
+    } catch (e) {
+      console.log('lulllzors', e)
+    }
+    console.log('---------------------------------------------')
+  }
 })
 
 // build('brisky-struct', (err, result) => {
