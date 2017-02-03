1086ffdfe8d0b5324ce78130da5919f69dea7efa
Jim de Beer <jim@vigour.io>
2017-02-03 12:43:41 +0100

    added correct split for imports

--- test/simple/a.js
+++ test/simple/a.js
@@ -1,16 +1,11 @@
 import bla from './b'
-// console.log(bla)
 var a = 1
-// now exports with vars etc
-// export default a
 
-// const y = { a: a }
-// var x = true
 const x = { a, bla }
 
-// export default {
-//   bla,
-//   a
-// }
+export default {
+  bla,
+  a
+}
 
-export default a
+// export default a
