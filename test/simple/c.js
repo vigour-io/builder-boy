56d9851d39598a1bbed1ad620552a98667681ad3
Jim de Beer <jim@vigour.io>
2017-02-04 17:17:29 +0100

    fixes

--- test/simple/c.js
+++ test/simple/c.js
@@ -1,6 +1,6 @@
 export default 'its the c!'
 // import file from './generated/file100.js'
-console.log(df!@#x)
+// console.log(df!@#x)
 const blurf = (val) => {
   return val + '!2222242121121!2lo'
 }
