bff1fded543c6e48df6555f465cf8304d9bc225e
Jim de Beer <jim@vigour.io>
2017-02-05 14:49:11 +0100

    fixed nested blocks

--- test/simple/a.js
+++ test/simple/a.js
@@ -1,26 +1,22 @@
-import c from './c'
+// import { set } from '../manipulate'
+const val = () => {}
+const set = () => {}
 
-const y = (x) => {
-  bla('hello')
-}
+const inject = (t, val, stamp) => typeof val === 'function'
+  ? val(t, val, stamp)
+  : set(t, val, stamp)
 
-const blam = {
-  c
-}
-
-export const blaxx = true
-
-const bla = (a) => {
-  console.log(c)
-  const x = {
-    c
+// export default
+const bla = (t, val, key, stamp) => {
+  var changed
+  if (Array.isArray(val)) {
+    for (let i = 0, len = val.length; i < len; i++) {
+      if (inject(t, val[i], stamp)) {
+        changed = true
+      }
+    }
+  } else {
+    changed = inject(t, val, stamp)
   }
+  return changed
 }
-
-// y(1)
-// var a = true
-console.log(c, bla())
-
-export const blar = c
-
-export { c }
