52dfdad12dfd36c87ff70a86ddda841d76c862d1
Jim de Beer <jim@vigour.io>
2017-02-05 14:55:58 +0100

    arror statement (non block)

--- test/simple/a.js
+++ test/simple/a.js
@@ -1,22 +1,3 @@
-// import { set } from '../manipulate'
-const val = () => {}
-const set = () => {}
+const bla = () => gurk()
+const gurk = () => true
 
-const inject = (t, val, stamp) => typeof val === 'function'
-  ? val(t, val, stamp)
-  : set(t, val, stamp)
-
-// export default
-const bla = (t, val, key, stamp) => {
-  var changed
-  if (Array.isArray(val)) {
-    for (let i = 0, len = val.length; i < len; i++) {
-      if (inject(t, val[i], stamp)) {
-        changed = true
-      }
-    }
-  } else {
-    changed = inject(t, val, stamp)
-  }
-  return changed
-}
