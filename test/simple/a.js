3f9c0ea48aecf78b4aa881318cfaf62e9f766b01
Jim de Beer <jim@vigour.io>
2017-02-05 15:48:48 +0100

    tests, clean up

--- test/simple/a.js
+++ test/simple/a.js
@@ -1,3 +1,23 @@
-const bla = () => gurk()
-const gurk = () => true
+const generator = (t, val, stamp) => iterator(t, val(t, stamp), stamp)
 
+const iterator = (t, iteratee, stamp, val) => {
+  const id = ++uid
+  if (!t.async) {
+    t.async = [ iteratee, stamp, id ]
+    // time out is a temp solution
+    // should work with bs.on ofcourse....
+    // bs.on(() => {
+    //   console.log('DO QUEUE')
+    //   queue(t)
+    // })
+    setTimeout(() => {
+      // console.log('DO QUEUE')
+      queue(t)
+    })
+  } else {
+    t.async.push(iteratee, stamp, id)
+  }
+}
+
+// this is the probelmo
+export { iterator }
