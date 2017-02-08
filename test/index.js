ca0cfc58bc29626a73718fa19f52b3d47cec50e6
Jim de Beer <jim@vigour.io>
2017-02-08 16:58:20 +0100

    better walking

--- test/index.js
+++ test/index.js
@@ -2,39 +2,39 @@ const build = require('../')
 const fs = require('fs')
 
 var cnt = 0
-build('test/simple/a.js', (err, result) => {
-  if (err) {
-    console.log('error', err)
-    return
-  }
-  cnt++
-  console.log(result.node)
-  fs.writeFileSync(`./test/simple/dist/${cnt}.js`, result.node)
-  console.log('\n\n\ngo run script!!!!\n')
-  try {
-    require(`./simple/dist/${cnt}.js`)
-  } catch (e) {
-    console.log(e)
-  }
-})
-
-// build('../brisky-struct/src/index.js', (err, result) => {
-//   console.log('hello wtf....')
+// build('test/simple/a.js', (err, result) => {
 //   if (err) {
-//     // console.log('.....ERROR', !!result, cnt, err)
+//     console.log('error', err)
 //     return
-//   } else {
-//     cnt++
-//     fs.writeFileSync(`./test/real/dist/${cnt}.js`, result.node)
-//     console.log('---------------------------------------------')
-//     try {
-//       require(`./real/dist/${cnt}.js`)
-//     } catch (e) {
-//       console.log('lulllzors', e)
-//     }
-//     console.log('---------------------------------------------')
+//   }
+//   cnt++
+//   console.log(result.node)
+//   fs.writeFileSync(`./test/simple/dist/${cnt}.js`, result.node)
+//   console.log('\n\n\ngo run script!!!!\n')
+//   try {
+//     require(`./simple/dist/${cnt}.js`)
+//   } catch (e) {
+//     console.log(e)
 //   }
 // })
+
+build('../brisky-struct/src/index.js', (err, result) => {
+  console.log('hello wtf....')
+  if (err) {
+    // console.log('.....ERROR', !!result, cnt, err)
+    return
+  } else {
+    cnt++
+    fs.writeFileSync(`./test/real/dist/${cnt}.js`, result.node)
+    console.log('---------------------------------------------')
+    try {
+      console.log(require(`./real/dist/${cnt}.js`))
+    } catch (e) {
+      console.log('lulllzors', e)
+    }
+    console.log('---------------------------------------------')
+  }
+})
 
 // build('./test/real/index.js', (err, result) => {
 //   console.log('hello wtf....')
