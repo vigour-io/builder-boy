830afcdd1651970cf7ee270e9b250c014330052b
Jim de Beer <jim@vigour.io>
2017-02-09 16:52:01 +0100

    fiddles

--- test/simple/require.js
+++ test/simple/require.js
@@ -1,6 +1,6 @@
-// var x = require('./c')
+var x = require('./c')
 
-const { stamp, y, bla: blurf } = require('./x')
+const { stamp, y, bla: blurf, yuz } = require('./x')
 
 console.log('bla', stamp.create(), y, blurf)
 
