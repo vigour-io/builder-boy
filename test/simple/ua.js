fedd9d59554bfd1c11063653317639e2378cfe85
Jim de Beer <jim@vigour.io>
2017-02-12 17:12:31 +0100

    correct ua parse for cases

--- test/simple/ua.js
+++ test/simple/ua.js
@@ -1,6 +1,6 @@
 // import { device, platform as blurf, version, browser } from 'vigour-ua/navigator'
 
-const { platform, device: smurt, version } = require('vigour-ua/navigator')
+const { a, b, x, y, z, d } = require('vigour-ua/navigator')
 // const x = require('vigour-ua/navigator')
 // import y from 'vigour-ua/navigator'
 
@@ -14,18 +14,33 @@ const { platform, device: smurt, version } = require('vigour-ua/navigator')
 
 // }
 
-if (
-  x === true &&
-  (smurt === 'bla' && version > 10) ||
-  smurt === 'turd' ||
-  smurt === 'bla' && (platform === 'ios' || version < 5)
-) {
+// if (
+//   x === true &&
+//   (smurt === 'bla' && version > 10) ||
+//   smurt === 'turd' ||
+//   smurt === 'bla' && (platform !== 'ios' || version < 5)
+// ) {
+//   if (platform === 'tv') {
+
+//   } else {
+//     // will become platform !== tv || version =< 1 || version >= -100
+//   }
+// } else {
+
+// }
+
+if (z === true || ((a === true || b === true) && x < 10)) {
+
+} else {
 
 }
 
+// if (platform !== 'ios') {
+//   if (platform === 'tv' && version > 10) {
+
+//   } else {
+//     // will become platform !== tv || version =< 1 || version >= -100
+//   }
+// }
+
 // result has to be
-
-[
-
-
-]
