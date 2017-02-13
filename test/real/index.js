825201d3bc5943cad1f6b22d02c8ddcad39089d3
Jim de Beer <jim@vigour.io>
2017-02-13 16:35:35 +0100

    detect if es6

--- test/real/index.js
+++ test/real/index.js
@@ -1,3 +1,20 @@
-// import { struct } from 'brisky-struct'
-import h from 'hub.js'
-// console.log(struct)
+// // import { struct } from 'brisky-struct'
+// import h from 'hub.js'
+// // console.log(struct)
+// var bla = `x`
+var bla = function * () {
+
+}
+// let x = 1
+// const y = 2
+// var bla = () => {}
+// var blax = { x }
+// var y = 2
+// var blax = { [y]: 2 }
+
+// var { x: xy } = { x: true }
+// var bla = `blurf`
+
+// var x  = { x: true }
+
+// [a, ...rest] = [1, 2, 3]
