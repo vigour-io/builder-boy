23efcf7fc6a1b2ed556326d825d08baf81e00a7b
Jim de Beer <jim@vigour.io>
2017-02-13 20:46:04 +0100

    added fixes

--- test/real/render.js
+++ test/real/render.js
@@ -1,71 +1,75 @@
-import br from 'brisky-render'
-// import hub from '../../../hub.js'
-const bla = require('./bla.json')
+// import br from 'brisky-render'
+// // import hub from '../../../hub.js'
+// const bla = require('./bla.json')
 
-// const exotic = require('./exotic')
-// console.log(exotic)
+// // const exotic = require('./exotic')
+// // console.log(exotic)
 
-// // const fs = require('fs')
+// // // const fs = require('fs')
 
-// const state = {
-//   hello: 'hello what?'
-// }
+// // const state = {
+// //   hello: 'hello what?'
+// // }
 
-const x = new Promise((resolve, reject) => {
+// const x = new Promise((resolve, reject) => {
 
-})
+// })
 
-fetch('http://google.com', () => {})
+// fetch('http://google.com', () => {})
 
-console.log('hello')
+// console.log('hello')
 
 
-// const blax = function * () {
-//   yield 'poop'
-// }
+const blax = function * () {
+  yield 'poop'
+  yield 'yuz'
+}
 
-// console.log(blax())
+const it = blax()
+for (var i of it) {
+  console.log('?', i)
+}
 // console.log(x())
 // var blax = new Promise()
 // fetch('http://google.com')
 
-const state = {
-  hello: 'HELLO!1'
-}
+// const state = {
+//   hello: 'HELLO!1'
+// }
 
-// // // console.log(state
+// // // // console.log(state
 
-document.body.appendChild(br.render({
-  text: { $: 'hello' },
-  bla: {
-    text: JSON.stringify(bla, false, 2),
-    style: {
-      marginTop: '15px',
-      fontSize: '20px'
-    }
-  },
-  style: {
-    color: 'rgb(20, 20, 20)',
-    padding: '15px',
-    margin: '0 auto',
-    marginTop: '150px',
-    background: '#ee',
-    borderRadius: '15px',
-    transform: {
-      rotate: 0
-    },
-    fontSize: '50px',
-    textAlign: 'center',
-    fontFamily: 'helvetica'
-  },
-  on: {
-    click: () => {
-      console.log('do it!')
-    }
-  }
-}, state))
+// document.body.appendChild(br.render({
+//   text: { $: 'hello' },
+//   bla: {
+//     text: JSON.stringify(bla, false, 2),
+//     style: {
+//       marginTop: '15px',
+//       fontSize: '20px'
+//     }
+//   },
+//   style: {
+//     color: 'rgb(20, 20, 20)',
+//     padding: '15px',
+//     margin: '0 auto',
+//     marginTop: '150px',
+//     background: '#ee',
+//     borderRadius: '15px',
+//     transform: {
+//       rotate: 0
+//     },
+//     fontSize: '50px',
+//     textAlign: 'center',
+//     fontFamily: 'helvetica'
+//   },
+//   on: {
+//     click: () => {
+//       console.log('do it!')
+//     }
+//   }
+// }, state))
 
-// // setInterval(() => {
-// //   window.location.reload()
-// // }, 200)
-// import { struct } from 'brisky-struct'
+// // // setInterval(() => {
+// // //   window.location.reload()
+// // // }, 200)
+// // import { struct } from 'brisky-struct'
