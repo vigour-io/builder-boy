20063988839ddf724a4eb83b4165ce89a346a909
Jim de Beer <jim@vigour.io>
2017-02-13 19:37:27 +0100

    add test

--- test/real/index.js
+++ test/real/index.js
@@ -1,24 +1,55 @@
-// // import { struct } from 'brisky-struct'
-// import h from 'hub.js'
-// // console.log(struct)
-// var bla = `x`
-// var bla = function * () {
+'use strict';
 
-// }
-// var promise = new Promise()
-// const bla = require('node-fetch')
-fetch('http://google.com', () => {})
+if (!global.briskystamp) {
+  'use strict';
+  var exports$1 = {};
+  const ts = exports$1.ts = 535673248076;
+  var tsInProgress = false;
+  var cnt = 0;
+  var d;
+  var on;
 
-// let x = 1
-// const y = 2
-// var bla = () => {}
-// var blax = { x }
-// var y = 2
-// var blax = { [y]: 2 }
+  global.briskystamp = exports$1;
+  exports$1.inProgress = false;
+  exports$1.offset = 0;
 
-// var { x: xy } = { x: true }
-// var bla = `blurf`
+  const ms = () => {
+    if (!tsInProgress) {
+      cnt = 0;
+      d = Date.now() - ts + exports$1.offset;
+      tsInProgress = true;
+      setTimeout(() => { tsInProgress = false; });
+    } else {
+      d += ++cnt / 9999;
+    }
+    return d
+  };
 
-// var x  = { x: true }
+  exports$1.create = override => override || ms();
 
-// [a, ...rest] = [1, 2, 3]
+  exports$1.on = fn => {
+    if (!on) {
+      on = [ fn ];
+    } else {
+      on.push(fn);
+    }
+  };
+
+  exports$1.clear = () => { on = false; }; // rename this to stop
+
+  exports$1.close = () => {
+    if (on && !exports$1.inProgress) {
+      exports$1.inProgress = true;
+      for (let i = 0; i < on.length; i++) { on[i](); }
+      exports$1.inProgress = on = false;
+    }
+  };
+
+  exports$1.parse = stamp => {
+    return stamp > 1e6 ? (stamp + ts) : stamp
+  };
+}
+
+var index = global.briskystamp;
+
+module.exports = index;
