7ef0117b7557a3997ee2393ef01aaf5609f2fd27
Jim de Beer <jim@vigour.io>
2017-02-14 19:40:57 +0100

    added virtual file support

--- test/index.js
+++ test/index.js
@@ -19,24 +19,41 @@ var cnt = 0
 //   }
 // })
 
-build('../brisky-struct/src/index.js', (err, result) => {
+build({ 'test/simple/bla.js': { virtual: true, code: 'console.log("dicks!")' } }, (err, result) => {
+  console.log('??????????')
   if (err) {
-    // console.log('.....ERROR', !!result, cnt, err)
+    console.log('error', err)
     return
-  } else {
-    cnt++
-    fs.writeFileSync(`./test/real/dist/${cnt}.js`, result.node)
-
-    console.log('---------------------------------------------')
-    try {
-      console.log(require(`./real/dist/${cnt}.js`))
-    } catch (e) {
-      console.log('lulllzors', e)
-    }
-    console.log('---------------------------------------------')
-  // })
+  }
+  cnt++
+  console.log(result.node)
+  fs.writeFileSync(`./test/simple/dist/${cnt}.js`, result.node)
+  console.log('\n\n\ngo run script!!!!\n')
+  try {
+    require(`./simple/dist/${cnt}.js`)
+  } catch (e) {
+    console.log(e)
   }
 })
+
+// build('../brisky-struct/src/index.js', (err, result) => {
+//   if (err) {
+//     // console.log('.....ERROR', !!result, cnt, err)
+//     return
+//   } else {
+//     cnt++
+//     fs.writeFileSync(`./test/real/dist/${cnt}.js`, result.node)
+
+//     console.log('---------------------------------------------')
+//     try {
+//       console.log(require(`./real/dist/${cnt}.js`))
+//     } catch (e) {
+//       console.log('lulllzors', e)
+//     }
+//     console.log('---------------------------------------------')
+//   // })
+//   }
+// })
 
 // build file as input
 // build('./test/real/render.js', (err, result) => {
