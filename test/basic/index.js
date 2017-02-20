eac11dcecf52bff562d45ecadfe946cf26fbdd08
Jim de Beer <jim@vigour.io>
2017-02-20 12:22:39 +0100

    correct add and id mechanism

--- test/basic/index.js
+++ test/basic/index.js
@@ -1,9 +1,15 @@
 const build = require('../../')
 const test = require('tape') // or ava? its node only
+const boy = require('../../lib/boy')
 
 test('simple', t => {
   console.log('?')
   build('./test/basic/a.js', (err, results) => {
     console.log('-----', err)
   })
+
+  // boy.add({
+  //   require: './a.js',
+  //   from: // has to be a file
+  // })
 })
