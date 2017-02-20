523355a3fdca4b0bbbd3d7e559e95ab4acd092c3
Jim de Beer <jim@vigour.io>
2017-02-20 15:45:14 +0100

    start of compile

--- test/basic/index.js
+++ test/basic/index.js
@@ -1,15 +1,17 @@
 const build = require('../../')
 const test = require('tape') // or ava? its node only
-const boy = require('../../lib/boy')
+// const boy = require('../../lib/boy')
 
 test('simple', t => {
   console.log('?')
-  setTimeout(() => {
-    console.log('-------------')
+  // setTimeout(() => {
+    // console.log('-------------')
     // console.log(boy.map(val => '---> ' + (val.result ? val.result.compute() : '----')))
-  }, 500)
+  // }, 500)
   build('./test/basic/a.js', (err, results) => {
-    console.log('-----', err)
+    console.log('-------------')
+    if (err) console.log('err', err)
+    console.log(results)
   })
 
   // boy.add({
