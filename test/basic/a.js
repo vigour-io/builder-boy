ca9fd0065d750924281b564ee5729f068e5980d4
Jim de Beer <jim@vigour.io>
2017-02-21 11:52:18 +0100

    link resolvement

--- test/basic/a.js
+++ test/basic/a.js
@@ -1,7 +1,8 @@
 import { create } from 'brisky-stamp'
+import { create as flurps } from 'brisky-struct'
 import b from './b'
 
-console.log(b, create())
+console.log(b, create(), flurps())
 
-// async function bek () { // <-- external
-// }
+async function bek () { // <-- external
+}
