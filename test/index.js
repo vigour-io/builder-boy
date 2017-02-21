f661c7f64f4f612e044f4ba6471b5d23cd13f203
Jim de Beer <jim@vigour.io>
2017-02-21 17:20:04 +0100

    clean up

--- test/index.js
+++ test/index.js
@@ -1,2 +1,21 @@
-require('./basic')
-require('./polyfill')
+const build = require('../')
+const test = require('tape')
+const { testBuild } = require('./util')
+
+test('simple', t => {
+  build('./test/basic/a.js', { nowatch: true }, (err, results, boy) => {
+    if (!err) {
+      testBuild('basic', results, t)
+      t.end()
+    }
+  })
+})
+
+test('polyfill', t => {
+  build('./test/polyfill/a.js', { nowatch: true }, (err, results, boy) => {
+    if (!err) {
+      testBuild('polyfill', results, t)
+      t.end()
+    }
+  })
+})
