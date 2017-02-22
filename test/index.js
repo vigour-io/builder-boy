38e2863d69eb8e0f38081db385158e2d25dc1025
Jim de Beer <jim@vigour.io>
2017-02-22 11:12:07 +0100

    re-enable tests

--- test/index.js
+++ test/index.js
@@ -2,23 +2,23 @@ const build = require('../')
 const test = require('tape')
 const { testBuild } = require('./util') //eslint-disable-line
 
-// test('simple', t => {
-//   build('./test/basic/a.js', { nowatch: true }, (err, results, boy) => {
-//     if (!err) {
-//       testBuild('basic', results, t)
-//       t.end()
-//     }
-//   })
-// })
+test('simple', t => {
+  build('./test/basic/a.js', { nowatch: true }, (err, results, boy) => {
+    if (!err) {
+      testBuild('basic', results, t)
+      t.end()
+    }
+  })
+})
 
-// test('polyfill', t => {
-//   build('./test/polyfill/a.js', { nowatch: true }, (err, results, boy) => {
-//     if (!err) {
-//       testBuild('polyfill', results, t)
-//       t.end()
-//     }
-//   })
-// })
+test('polyfill', t => {
+  build('./test/polyfill/a.js', { nowatch: true }, (err, results, boy) => {
+    if (!err) {
+      testBuild('polyfill', results, t)
+      t.end()
+    }
+  })
+})
 
 test('env - override', t => {
   build('./test/env/index.js', {
@@ -46,82 +46,82 @@ test('env - inherit', t => {
   })
 })
 
-// test('ua', t => {
-//   build('./test/ua/index.js', {
-//     nowatch: true,
-//     targets: [ 'node' ],
-//     inline: [ 'brisky-stamp' ]
-//   }, (err, results, boy) => {
-//     if (!err) {
-//       testBuild('ua', results.ua.node.builds, t)
-//       t.end()
-//     }
-//   })
-// })
+test('ua', t => {
+  build('./test/ua/index.js', {
+    nowatch: true,
+    targets: [ 'node' ],
+    inline: [ 'brisky-stamp' ]
+  }, (err, results, boy) => {
+    if (!err) {
+      testBuild('ua', results.ua.node.builds, t)
+      t.end()
+    }
+  })
+})
 
-// test('virtual', t => {
-//   build({
-//     'virtual': {
-//       virtual: true,
-//       code: 'export default \'hahaha\''
-//     }
-//   }, { nowatch: true }, (err, results, boy) => {
-//     if (!err) {
-//       build('./test/virtual/index.js', { nowatch: true }, (err, results, boy) => {
-//         if (!err) {
-//           testBuild('virtual', results, t)
-//           t.end()
-//         }
-//       })
-//     }
-//   })
-// })
+test('virtual', t => {
+  build({
+    'virtual': {
+      virtual: true,
+      code: 'export default \'hahaha\''
+    }
+  }, { nowatch: true }, (err, results, boy) => {
+    if (!err) {
+      build('./test/virtual/index.js', { nowatch: true }, (err, results, boy) => {
+        if (!err) {
+          testBuild('virtual', results, t)
+          t.end()
+        }
+      })
+    }
+  })
+})
 
-// test('package - error', t => {
-//   build('./test/pkg/index.js', {
-//     nowatch: true,
-//     targets: [ 'node' ]
-//   }, (err, results, boy) => {
-//     if (err) {
-//       t.ok(err.file.indexOf('package.json') !== -1, 'err gets file path')
-//       t.end()
-//     }
-//   })
-// })
+test('package - error', t => {
+  build('./test/pkg/index.js', {
+    nowatch: true,
+    targets: [ 'node' ]
+  }, (err, results, boy) => {
+    if (err) {
+      t.ok(err.file.indexOf('package.json') !== -1, 'err gets file path')
+      t.end()
+    }
+  })
+})
 
-// test('json', t => {
-//   build('./test/json/index.js', {
-//     nowatch: true,
-//     targets: [ 'node' ]
-//   }, (err, results, boy) => {
-//     if (!err) {
-//       testBuild('json', results, t)
-//       t.end()
-//     }
-//   })
-// })
+test('json', t => {
+  build('./test/json/index.js', {
+    nowatch: true,
+    targets: [ 'node' ]
+  }, (err, results, boy) => {
+    if (!err) {
+      testBuild('json', results, t)
+      t.end()
+    }
+  })
+})
 
-// test('error', t => {
-//   build('./test/error/index.js', {
-//     nowatch: true,
-//     targets: [ 'node' ]
-//   }, (err, results, boy) => {
-//     if (err) {
-//       t.equal(err.message, 'Cannot find module "hello" (1:0)')
-//       t.end()
-//     }
-//   })
-// })
+test('error', t => {
+  build('./test/error/index.js', {
+    nowatch: true,
+    targets: [ 'node' ]
+  }, (err, results, boy) => {
+    if (err) {
+      t.equal(err.message, 'Cannot find module "hello" (1:0)')
+      t.end()
+    }
+  })
+})
 
-// test('nested', t => {
-//   build('./test/nested/index.js', {
-//     nowatch: true,
-//     targets: [ 'inline' ]
-//   }, (err, results, boy) => {
-//     console.log(results.inline)
-//     if (!err) {
-//       testBuild('nested', results, t)
-//       t.end()
-//     }
-//   })
-// })
+test('nested', t => {
+  build('./test/nested/index.js', {
+    nowatch: true,
+    targets: [ 'inline' ]
+  }, (err, results, boy) => {
+    console.log(results.inline)
+    if (!err) {
+      testBuild('nested', results, t)
+      t.end()
+    }
+  })
+})
