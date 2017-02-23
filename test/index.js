08cdcf9745c546f4190c78c312aec7df7afc77ce
Jim de Beer <jim@vigour.io>
2017-02-23 13:45:15 +0100

    clean up

--- test/index.js
+++ test/index.js
@@ -4,131 +4,131 @@ const { testBuild } = require('./util') //eslint-disable-line
 
 const fs = require('fs')
 
-// build('./test/jsx/index.js', { target: 'inline' }, (err, { inline }) => {
-//   if (!err) {
-//     fs.writeFileSync('./test/jsx/dist/bla.js', inline)
-//   }
+build('./test/jsx/index.js', { target: 'inline' }, (err, { inline }) => {
+  if (!err) {
+    fs.writeFileSync('./test/jsx/dist/bla.js', inline)
+  }
+})
+
+// test('simple', t => {
+//   build('./test/basic/a.js', { nowatch: true }, (err, results, boy) => {
+//     if (!err) {
+//       testBuild('basic', results, t)
+//       t.end()
+//     }
+//   })
 // })
 
-test('simple', t => {
-  build('./test/basic/a.js', { nowatch: true }, (err, results, boy) => {
-    if (!err) {
-      testBuild('basic', results, t)
-      t.end()
-    }
-  })
-})
+// test('polyfill', t => {
+//   build('./test/polyfill/a.js', { nowatch: true }, (err, results, boy) => {
+//     if (!err) {
+//       testBuild('polyfill', results, t)
+//       t.end()
+//     }
+//   })
+// })
 
-test('polyfill', t => {
-  build('./test/polyfill/a.js', { nowatch: true }, (err, results, boy) => {
-    if (!err) {
-      testBuild('polyfill', results, t)
-      t.end()
-    }
-  })
-})
+// test('env - override', t => {
+//   build('./test/env/index.js', {
+//     nowatch: true,
+//     targets: [ 'inline' ],
+//     env: { beurs: 'ha!' }
+//   }, (err, results, boy) => {
+//     if (!err) {
+//       testBuild('env', results, t)
+//       t.end()
+//     }
+//   })
+// })
 
-test('env - override', t => {
-  build('./test/env/index.js', {
-    nowatch: true,
-    targets: [ 'inline' ],
-    env: { beurs: 'ha!' }
-  }, (err, results, boy) => {
-    if (!err) {
-      testBuild('env', results, t)
-      t.end()
-    }
-  })
-})
+// test('env - inherit', t => {
+//   process.env.beurs = 'ha!'
+//   build('./test/env/index.js', {
+//     nowatch: true,
+//     targets: [ 'inline' ]
+//   }, (err, results, boy) => {
+//     if (!err) {
+//       testBuild('env', results, t)
+//       t.end()
+//     }
+//   })
+// })
 
-test('env - inherit', t => {
-  process.env.beurs = 'ha!'
-  build('./test/env/index.js', {
-    nowatch: true,
-    targets: [ 'inline' ]
-  }, (err, results, boy) => {
-    if (!err) {
-      testBuild('env', results, t)
-      t.end()
-    }
-  })
-})
+// test('ua', t => {
+//   build('./test/ua/index.js', {
+//     nowatch: true,
+//     targets: [ 'node' ],
+//     inline: [ 'brisky-stamp' ]
+//   }, (err, results, boy) => {
+//     if (!err) {
+//       testBuild('ua', results.ua.node.builds, t)
+//       t.end()
+//     }
+//   })
+// })
 
-test('ua', t => {
-  build('./test/ua/index.js', {
-    nowatch: true,
-    targets: [ 'node' ],
-    inline: [ 'brisky-stamp' ]
-  }, (err, results, boy) => {
-    if (!err) {
-      testBuild('ua', results.ua.node.builds, t)
-      t.end()
-    }
-  })
-})
+// test('virtual', t => {
+//   build({
+//     'virtual': {
+//       virtual: true,
+//       code: 'export default \'hahaha\''
+//     }
+//   }, { nowatch: true }, (err, results, boy) => {
+//     if (!err) {
+//       build('./test/virtual/index.js', { nowatch: true }, (err, results, boy) => {
+//         if (!err) {
+//           testBuild('virtual', results, t)
+//           t.end()
+//         }
+//       })
+//     }
+//   })
+// })
 
-test('virtual', t => {
-  build({
-    'virtual': {
-      virtual: true,
-      code: 'export default \'hahaha\''
-    }
-  }, { nowatch: true }, (err, results, boy) => {
-    if (!err) {
-      build('./test/virtual/index.js', { nowatch: true }, (err, results, boy) => {
-        if (!err) {
-          testBuild('virtual', results, t)
-          t.end()
-        }
-      })
-    }
-  })
-})
+// test('package - error', t => {
+//   build('./test/pkg/index.js', {
+//     nowatch: true,
+//     targets: [ 'node' ]
+//   }, (err, results, boy) => {
+//     if (err) {
+//       t.ok(err.file.indexOf('package.json') !== -1, 'err gets file path')
+//       t.end()
+//     }
+//   })
+// })
 
-test('package - error', t => {
-  build('./test/pkg/index.js', {
-    nowatch: true,
-    targets: [ 'node' ]
-  }, (err, results, boy) => {
-    if (err) {
-      t.ok(err.file.indexOf('package.json') !== -1, 'err gets file path')
-      t.end()
-    }
-  })
-})
+// test('json', t => {
+//   build('./test/json/index.js', {
+//     nowatch: true,
+//     targets: [ 'node' ]
+//   }, (err, results, boy) => {
+//     if (!err) {
+//       testBuild('json', results, t)
+//       t.end()
+//     }
+//   })
+// })
 
-test('json', t => {
-  build('./test/json/index.js', {
-    nowatch: true,
-    targets: [ 'node' ]
-  }, (err, results, boy) => {
-    if (!err) {
-      testBuild('json', results, t)
-      t.end()
-    }
-  })
-})
+// test('error', t => {
+//   build('./test/error/index.js', {
+//     nowatch: true,
+//     targets: [ 'node' ]
+//   }, (err, results, boy) => {
+//     if (err) {
+//       t.equal(err.message, 'Cannot find module "hello" (1:0)')
+//       t.end()
+//     }
+//   })
+// })
 
-test('error', t => {
-  build('./test/error/index.js', {
-    nowatch: true,
-    targets: [ 'node' ]
-  }, (err, results, boy) => {
-    if (err) {
-      t.equal(err.message, 'Cannot find module "hello" (1:0)')
-      t.end()
-    }
-  })
-})
-
-test('nested', t => {
-  build('./test/nested/index.js', {
-    nowatch: true,
-    targets: [ 'inline' ]
-  }, (err, results, boy) => {
-    if (!err) {
-      testBuild('nested', results, t)
-      t.end()
-    }
-  })
-})
+// test('nested', t => {
+//   build('./test/nested/index.js', {
+//     nowatch: true,
+//     targets: [ 'inline' ]
+//   }, (err, results, boy) => {
+//     if (!err) {
+//       testBuild('nested', results, t)
+//       t.end()
+//     }
+//   })
+// })
