3e2eb13fb794827134e2eca194ee4dc39eac89cd
Jim de Beer <jim@vigour.io>
2017-02-23 15:33:54 +0100

    good nested elem walker

--- test/jsx/text.jsx
+++ test/jsx/text.jsx
@@ -1,9 +1,8 @@
-const Blurfx = state => <div>{state.field.title.compute()}</div>
-
-
-// const Blurfx2 = function (state) {
-//   return <div>{state.field.title.compute()}</div>
-// }
-
+const Blurfx = state => <div>
+  <div>
+    static boy
+    <div>haha</div>
+  </div>
+</div>
 
 var $615976759 = Blurfx
