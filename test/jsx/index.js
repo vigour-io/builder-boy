1863d7a14b1a07ce86fce6b5ac6b4526d9b5a59f
Jim de Beer <jim@vigour.io>
2017-02-23 20:16:23 +0100

    break when returning true from walk

--- test/jsx/index.js
+++ test/jsx/index.js
@@ -1,9 +1,8 @@
 import { render } from 'brisky-render'
 import Text from './text.jsx'
 
-// const Text = { text: 'x' }
-
 document.body.appendChild(render(Text, {
   title: 'hello world!',
+  nested: { blur: 'hello' },
   color: 'blue'
 }))
