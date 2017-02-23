deb59f9f03ba7d44dc0009d1dc3680f65ea1cfe9
Jim de Beer <jim@vigour.io>
2017-02-23 14:29:14 +0100

    add block in walker

--- test/jsx/text.jsx
+++ test/jsx/text.jsx
@@ -1,3 +1,9 @@
 const Blurfx = state => <div>{state.field.title.compute()}</div>
 
+
+// const Blurfx2 = function (state) {
+//   return <div>{state.field.title.compute()}</div>
+// }
+
+
 var $615976759 = Blurfx
