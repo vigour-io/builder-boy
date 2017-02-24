743b15871051aca59ae7799a93ed635729fd4104
Jim de Beer <jim@vigour.io>
2017-02-24 11:12:34 +0100

    start of attr subscriptions

--- test/jsx/text.jsx
+++ test/jsx/text.jsx
@@ -1,5 +1,3 @@
-const cat = 'https://media1.popsugar-assets.com/files/thumbor/EYb5dO2AAuFKts5Vj6o8wUPLV_E/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2014/08/08/878/n/1922507/caef16ec354ca23b_thumb_temp_cover_file32304521407524949/i/Funny-Cat-GIFs.jpg'
-
 const flurps = 'hahahahahaha'
 
 const id = 'bye'
@@ -10,17 +8,14 @@ const Blurfx = state => <div>
       color: 'pink',
       // background: state.color.compute()
     }}>haha</div>
-    <img src={cat}/>
+    <img src={state.cat.compute()}/>
   </div>
-  <div>---{state.nested.blurf.compute() + '!!!!'}----</div>
-  <div key={id}>
-    hello: {flurps} !!!!!
-  </div>
+  <div>--- {'!!!' + state.nested.blurf.compute() + '!!!!'} ----</div>
+  <div key={id}>hello: {flurps} !!!!!</div>
   <a key='hello' />
-  <button onClick={({ state }) => {
-    state.nested.blurf.set(Math.random())
-    console.log('CLICK', state)
-  }}>click me!</button>
+  <button onClick={({ state }) => state.nested.blurf.set(Math.random())}>
+    click me!
+  </button>
   <div>
   {{
     blabs: <div>WOW MIND IS BLOWN</div>,
@@ -30,3 +25,6 @@ const Blurfx = state => <div>
 </div>
 
 var $615976759 = Blurfx
+
+// transpile objects in a fn with capitall as well then its complete
+// then you an mix everythign up
