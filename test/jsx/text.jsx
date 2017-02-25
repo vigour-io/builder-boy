a1049d1545d92f7c97fa138a7b592d69ee4f75e5
Jim de Beer <jim@vigour.io>
2017-02-25 13:40:38 +0100

    include right bound for compile

--- test/jsx/text.jsx
+++ test/jsx/text.jsx
@@ -10,23 +10,24 @@ const click = ({ state }) => state.set({ blurf: { real: false } })
 const Blurfx = yuz => <ul>
   <hr/>
   {yuz.list
-    // .filter(x =>
-    //   x.blurf.real.compute() &&
-    //   x.title.compute() === 'yes'
-    // )
-    // .slice(0, 3)
+    .filter(x => x.blurf.real.compute())
     // .sort((a, b) => {
-    //   return a.order.compute() > b.order.compute() ? -1 : 1
+    //   return a.key > b.key ? -1 : 1
     // })
-    .sort((a, b) => {
-      return a.key > b.key ? -1 : 1
-    })
-    .map(state => {
+    .map(yuz => {
       return <li onClick={click}>
-        yes: {s.emoji.compute()}
+        yes: {yuz.emoji.compute()}
       </li>
     }
   )}
 </ul>
 
 var $615976759 = Blurfx
+
+/*
+    // .slice(0, 3)
+    // .sort((a, b) => {
+    //   return a.order.compute() > b.order.compute() ? -1 : 1
+    // })
+*/
+
