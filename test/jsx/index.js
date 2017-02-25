3fb4d7e1c5b5dc930b5f047103ea7f2bc3869764
Jim de Beer <jim@vigour.io>
2017-02-25 19:31:03 +0100

    added switch

--- test/jsx/index.js
+++ test/jsx/index.js
@@ -3,7 +3,7 @@ import Text from './text.jsx'
 
 const cat = 'https://media1.popsugar-assets.com/files/thumbor/EYb5dO2AAuFKts5Vj6o8wUPLV_E/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2014/08/08/878/n/1922507/caef16ec354ca23b_thumb_temp_cover_file32304521407524949/i/Funny-Cat-GIFs.jpg'
 
-document.body.appendChild(render(Text, {
+document.body.appendChild(render({ smurt: Text }, {
   title: 'hello world!',
   nested: { blurf: 'hello' },
   color: 'blue',
--- test/jsx/text.jsx
+++ test/jsx/text.jsx
@@ -1,9 +1,37 @@
+
+const style = {
+  padding: '10px',
+  background: '#eee',
+  margin: '0 auto',
+  width: '100px',
+  height: '100px',
+  borderRadius: '50%'
+}
+
+const style2 = {
+  background: 'blue',
+  color: 'white',
+  margin: '0 auto',
+  width: '20px',
+  height: '20px',
+  textAlign: 'center',
+  padding: '10px'
+}
+
 const Blurfx = state => {
   console.log('lets add some swithcin\'')
-  if (state.condtion.compute() === 'ballz') {
-    return <div>hello</div>
+  if (state.condition.compute() === 'ballz') {
+    return <div style={style} onClick={({ state }) => state.set({
+      condition: 'no-ballz'
+    })}></div>
+  } else {
+    return <div style={style2} onClick={({ state }) => state.set({
+      condition: 'ballz'
+    })}>x</div>
   }
-  // add conditional foc
 }
+
+
+// const Blurfx = state => state.condition.compute() === 'ballz' && <div>X</div>
 
 var $615976759 = Blurfx
