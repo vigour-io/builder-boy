64a4947d7c30d2b8f98c59924d50379c3e9a18d3
Jim de Beer <jim@vigour.io>
2017-03-16 11:52:47 +0100

    re-enabled all tests

--- test/jsx-any/results/inline.js
+++ test/jsx-any/results/inline.js
@@ -1,9 +1,40 @@
 (function (global, process) { 
 var $1293301303_Div = {
   tag: 'div',
-  child0: {
-    type: 'text',
-    val: "s.title.compute()"
+  $: 'list.$any',
+  props: {
+    default: {
+      $switch: {
+        val: function (s) {
+          return s.title ?
+            "child0" :
+            "child1"
+        }
+      },
+      $: '$switch',
+      props: {
+        child0: {
+          tag: 'div',
+          child0: {
+            type: 'text',
+            $: 'title',
+            $transform: function ($val, s) {
+              return s.compute()
+            }
+          }
+        },
+        child1: {
+          tag: 'span',
+          child0: {
+            type: 'text',
+            $: true,
+            $transform: function ($val, s) {
+              return s.compute()
+            }
+          }
+        }
+      }
+    }
   }
 };
  })(window, {})
--- test/jsx-any/results/node.js
+++ test/jsx-any/results/node.js
@@ -1,7 +1,38 @@
 const $1293301303_Div = {
   tag: 'div',
-  child0: {
-    type: 'text',
-    val: `s.title.compute()`
+  $: 'list.$any',
+  props: {
+    default: {
+      $switch: {
+        val: (s) => {
+          return s.title ?
+            "child0" :
+            "child1"
+        }
+      },
+      $: '$switch',
+      props: {
+        child0: {
+          tag: 'div',
+          child0: {
+            type: 'text',
+            $: 'title',
+            $transform: ($val, s) => {
+              return s.compute()
+            }
+          }
+        },
+        child1: {
+          tag: 'span',
+          child0: {
+            type: 'text',
+            $: true,
+            $transform: ($val, s) => {
+              return s.compute()
+            }
+          }
+        }
+      }
+    }
   }
 }
