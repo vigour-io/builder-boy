c30ff0ddd44a4d6b98b9e0b5e18750b64c6cbe19
Mustafa Dokumaci <mustafa@foraa.com>
2017-03-17 17:26:39 +0100

    trying to test

--- test/ua/index.js
+++ test/ua/index.js
@@ -4,7 +4,7 @@ if (device === 'phone' || device === 'tablet' || device === 'tv') {
   console.log('phone or tablet')
 } else if (platform === 'windows' && browser === 'firefox') {
   console.log('firefox on windows non phone or tablet or tv')
-  console.log(version > 30 ? '30+' : '30-')
+  console.log(version > 20 ? '20+' : '20-')
 } else {
   console.log('other')
   console.log(version < 30 ? '30-' : '30+')
@@ -22,21 +22,23 @@ console.log('all')
 // == step 1
 
 // 1. device===phone||device===tablet||device===tv
-// 2. (device!==phone&&device!==tablet&&device!==tv)&&(platform===windows&&browser===firefox)
-// 3. (device!==phone&&device!==tablet&&device!==tv)&&(platform!==windows||browser!==firefox)&&version<30
-// 4. (device!==phone&&device!==tablet&&device!==tv)&&(platform!==windows||browser!==firefox)&&version>=30
+// 2. (device[$nin=phone,tablet,tv])&&(platform===windows&&browser===firefox)&&version>20
+// 3. (device[$nin=phone,tablet,tv])&&(platform===windows&&browser===firefox)&&version<=20
+// 4. (device[$nin=phone,tablet,tv])&&(platform!==windows||browser!==firefox)&&version<30
+// 5. (device[$nin=phone,tablet,tv])&&(platform!==windows||browser!==firefox)&&version>=30
 
 // 1. (device===tablet)&&(browser===chrome&&version>=40)
-// 2. (device===tablet)&&(browser===chrome&&version<40)
+// 2. (device===tablet)&&(browser!==chrome||version<40)
 // 3. (device!==tablet)
 
 // == step 2
 
 // 1. (device===tablet)&&(browser===chrome&&version>=40)
-// 2. (device===tablet)&&(browser===chrome&&version<40)
+// 2. (device===tablet)&&(browser!==chrome||version<40)
 // 3. (device===phone||device===tv)
-// 4. (device!==phone&&device!==tablet&&device!==tv)&&(platform===windows&&browser===firefox)
-// 5. (device!==phone&&device!==tablet&&device!==tv)&&(platform!==windows||browser!==firefox)&&version<30
-// 6. (device!==phone&&device!==tablet&&device!==tv)&&(platform!==windows||browser!==firefox)&&version>=30
+// 4. (device[$nin=phone,tablet,tv])&&(platform===windows&&browser===firefox)&&version>20
+// 5. (device[$nin=phone,tablet,tv])&&(platform===windows&&browser===firefox)&&version<=20
+// 6. (device[$nin=phone,tablet,tv])&&(platform!==windows||browser!==firefox)&&version<30
+// 7. (device[$nin=phone,tablet,tv])&&(platform!==windows||browser!==firefox)&&version>=30
 
 // browser, version, prefix, platform, device, webview
