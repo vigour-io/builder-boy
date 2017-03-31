ea132ebcf1a8109dd939524b15ab61ea1f6315f1
Mustafa Dokumaci <mustafa@foraa.com>
2017-03-31 14:42:54 +0200

    fix simplify

--- test/ua/basic/results/3501905669.js
+++ test/ua/basic/results/3501905669.js
@@ -1,4 +1,4 @@
-var $2244796395={"browser":"firefox","version":21,"prefix":"any","platform":"windows","device":"not-phone-tablet-tv","webview":"any"}
+var $2244796395={"browser":"firefox","version":21,"prefix":"any","platform":"windows","device":"not-phone-tablet","webview":"any"}
 ;
 
 if ($2244796395.device === 'phone' || $2244796395.device === 'tablet' || ($2244796395.device === 'tv' && $2244796395.platform === 'tizen')) {
