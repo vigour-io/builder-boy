2984769c84ce405cabf11de1fba9e63edfb6c361
Mustafa Dokumaci <mustafa@foraa.com>
2017-03-31 09:56:02 +0200

    separate tests

--- test/ua/index.js
+++ test/ua/index.js
@@ -1,22 +1,3 @@
-import { device, platform, browser, version } from 'vigour-ua/navigator'
-// import 'brisky-render'
-
-if (device === 'phone' || device === 'tablet' || (device === 'tv' && platform === 'tizen')) {
-  console.log('phone or tablet')
-} else if (platform === 'windows' && browser === 'firefox') {
-  console.log('firefox on windows non phone or tablet or tv')
-  console.log(version > 20 ? '20+' : '20-')
-} else {
-  console.log('other')
-  console.log(version < 30 ? '30-' : '30+')
-}
-
-if (device === 'tablet') {
-  console.log('any tablet')
-  if (browser === 'chrome' && version >= 40) {
-    console.log('chrome 40+ on any tablet')
-  }
-}
 
 if (version < 50) {
 
