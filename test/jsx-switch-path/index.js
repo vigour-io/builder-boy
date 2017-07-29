b75591be1d795f74f577563d9e49342e5d96752b
Jim de Beer <jim@vigour.io>
2017-07-29 19:08:13 +0200

    node v8 compat

--- test/jsx-switch-path/index.js
+++ test/jsx-switch-path/index.js
@@ -1,3 +1,16 @@
 const Div = props => props.title[0].compute() === 'bla'
   ? <div>ha!</div>
   : <span>ho!</span>
+
+const ActionButtonTv = props => <div style={{
+  backgroundColor: !props.root().menu.open.compute() && props.root().layer[0].items.columnFocusKey.compute() == 0 //eslint-disable-line
+  ? 'white'
+  : backgroundColor,
+  color: !props.root().menu.open.compute() && props.root().layer[0].items.columnFocusKey.compute() == 0 //eslint-disable-line
+  ? backgroundColor
+  : 'white',
+  display: 'table',
+  borderSpacing: '15rem',
+  fontSize: '24rem',
+  marginTop: '15rem'
+}} />
